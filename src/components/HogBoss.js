3d54385f80d982f22d5d154115f8201f46691bdb
Diana Jordan <dianacjordan@gmail.com>
2023-03-16 16:54:05 -0400

    complete lab

--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function HogBoss() {
   const [eyeColor, setEyeColor] = useState("blue");
@@ -9,6 +9,8 @@ function HogBoss() {
   function handleChangeEyeColor(e) {
     setEyeColor(e.target.value);
   }
+
+  
 
   return (
     <div>
